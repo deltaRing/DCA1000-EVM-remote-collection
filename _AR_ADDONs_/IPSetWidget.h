bea49cc7d41eed5badad6c2251e3fee98b5bfcf5
e1ysium <43900243+deltaRing@users.noreply.github.com>
2023-01-10 00:42:04 +0800

    Add files via upload

--- _AR_ADDONs_/IPSetWidget.h
+++ _AR_ADDONs_/IPSetWidget.h
@@ -1,1 +1,30 @@
+#pragma once
 
+#include <mutex>
+#include <QWidget>
+#include <QHostAddress>
+#include "ui_IPSetWidget.h"
+#include "../_AR_define_/_net_work_define_.h"
+#include "../_AR_include_/_AR_UI_INFO_SUBSYSTEM_.h"
+
+extern QString IP;
+extern QString Port;
+
+class IPSetWidget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	IPSetWidget(QWidget *parent = nullptr);
+	~IPSetWidget();
+
+private:
+	Ui::IPSetWidgetClass ui;
+
+signals:
+	void ip_changed();
+
+public slots:
+	void click_abort(); // 取消IP设置
+	void click_confirm(); // 确定IP设置
+};
